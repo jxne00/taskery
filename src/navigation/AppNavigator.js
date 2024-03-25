d8727a444a6911b6a9b583fce3f47ee951f6954b
june <86581908+jxne00@users.noreply.github.com>
2024-03-25 15:56:41 +0800

    Add padding for Android compatibility

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import { View, Platform, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import { AuthStack } from './StackNavigator';
 import BottomTabNav from './BottomTabNav';
 
 const Stack = createStackNavigator();
 
 /**
+ * Add padding to the top of the app for android devices
+ * to avoid status bar overlapping the screen content
+ */
+const androidPadding = (Component) => (props) => {
+    if (Platform.OS === 'android') {
+        return (
+            <View style={{ flex: 1, paddingTop: StatusBar.currentHeight }}>
+                <Component {...props} />
+            </View>
+        );
+    }
+
+    return <Component {...props} />;
+};
+
+const PaddedContainer = androidPadding(NavigationContainer);
+
+/**
  * The main navigator for the app containing auth and home stacks
  */
 const AppNavigator = () => {
     return (
-        <NavigationContainer>
+        <PaddedContainer>
             <Stack.Navigator
                 initialRouteName="AuthStack"
                 screenOptions={{
@@ -22,7 +40,7 @@ const AppNavigator = () => {
                 <Stack.Screen name="AuthStack" component={AuthStack} />
                 <Stack.Screen name="HomeTabs" component={BottomTabNav} />
             </Stack.Navigator>
-        </NavigationContainer>
+        </PaddedContainer>
     );
 };
 
